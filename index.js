efbddc15d5d498ca122d96109024fc6bc87a0fb1
dereknguyenm <48599167+dereknguyenm@users.noreply.github.com>
2023-12-28 18:42:08 -0800

    Use querySelector instead of getByID
    -Defines css of class with . in front

--- index.js
+++ index.js
@@ -2,26 +2,32 @@
 // Set their values to a random number between 2-11
 let firstCard = 2
 let secondCard = 11
-
 let sum = firstCard + secondCard
+
 let hasBlackJack = false
 let isAlive = true
 
 let message = ""
+let messageEl = document.getElementById("message-el")
+
+// another more powerful tool is using querySelector instead of get ElementbyID
+// the . means for a class and the # means by ID
+let sumEl = document.querySelector(".sum-el")
 
 
 function startGame()
 {
     // 2. Reassign the message variable to the string we're logging out
+    sumEl.textContent = "Sum:" + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
 
-    console.log(message)
+    messageEl.textContent = message
 }
